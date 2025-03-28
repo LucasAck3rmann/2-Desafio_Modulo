d159d4c1903474d4b996b96ed77f4460cf3652bd
Lucas Ackermann <ackermannlucas25@gmail.com>
2025-03-28 15:32:49 -0300

    Configuração correta do vite

--- desafio-github/vite.config.ts
+++ desafio-github/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+export const viteConfig = defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.4.4',
+    host: true,
   },
 });
